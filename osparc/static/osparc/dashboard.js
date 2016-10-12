97937e45c77ce6152c071a1b33b761521b9f5cc2
Doug Bergh <dougbergh@gmail.com>
2016-10-12 14:05:57 -0700

    PR KPI

--- osparc/static/osparc/dashboard.js
+++ osparc/static/osparc/dashboard.js
@@ -121,10 +121,10 @@ var osparc_dashboard = function() {
                 document.getElementById('yield_min').innerHTML = result['MonthlyYield']['min'];
                 document.getElementById('yield_max').innerHTML = result['MonthlyYield']['max'];
 
-                // document.getElementById('pr_mean').innerHTML = result['PerformanceRatio']['mean'];
-                // document.getElementById('pr_med').innerHTML = result['PerformanceRatio']['median'];
-                // document.getElementById('pr_min').innerHTML = result['PerformanceRatio']['min'];
-                // document.getElementById('pr_max').innerHTML = result['PerformanceRatio']['max'];
+                document.getElementById('pr_mean').innerHTML = result['PerformanceRatio']['mean'];
+                document.getElementById('pr_med').innerHTML = result['PerformanceRatio']['median'];
+                document.getElementById('pr_min').innerHTML = result['PerformanceRatio']['min'];
+                document.getElementById('pr_max').innerHTML = result['PerformanceRatio']['max'];
 
                 document.getElementById('stor_mean').innerHTML = result['StorageCapacity']['mean'];
                 document.getElementById('stor_med').innerHTML = result['StorageCapacity']['median'];
