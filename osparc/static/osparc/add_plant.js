aab45b1c31c2374120511fb6009117c40e7656ca
Doug Bergh <dougbergh@gmail.com>
2016-11-01 15:33:55 -0700

    add plant from json

--- osparc/static/osparc/add_plant.js
+++ osparc/static/osparc/add_plant.js
@@ -1,21 +1,6 @@
 var osparc_addplant = function() {
 
-    function addPlant() {
-
-        var inputArray = $('input').serializeArray();
-
-        var postArray = {};
-        inputArray.forEach(function (item) {
-              if (postArray[item.name] !== undefined) {
-                if (!postArray[item.name].push) {
-                    postArray[item.name] = [postArray[item.name]];
-                }
-                postArray[item.name].push(item.value || '');
-            } else {
-                postArray[item.name] = item.value || '';
-            }
-        });
-
+    function postToServer(postArray) {
         url = "http://localhost:8001/api/plants";
 
         $.ajax( {
@@ -34,11 +19,38 @@ var osparc_addplant = function() {
         }
         });
 
-
         return true;
     }
 
+    function addPlant() {
+
+        var inputArray = $('input').serializeArray();
+
+        var postArray = {};
+        inputArray.forEach(function (item) {
+              if (postArray[item.name] !== undefined) {
+                if (!postArray[item.name].push) {
+                    postArray[item.name] = [postArray[item.name]];
+                }
+                postArray[item.name].push(item.value || '');
+            } else {
+                postArray[item.name] = item.value || '';
+            }
+        });
+
+        return postToServer(postArray);
+    }
+
+    function uploadPlant() {
+        var str = $('textarea')[0].value;
+
+        var jsonArray = JSON.parse(str);
+
+        return postToServer(jsonArray);
+    }
+
     return {
-        addPlant:addPlant
+        addPlant:addPlant,
+        uploadPlant:uploadPlant
     }
 }();
